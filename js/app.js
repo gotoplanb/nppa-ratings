7f4d274a5f396708f34649c626b6f80352f846c3
Mike Davis <mikedavis@smartmediacreative.com>
2014-05-05 17:23:31 -0400

    App router and basic photo template.

--- js/app.js
+++ js/app.js
@@ -4,12 +4,83 @@
 
   window.NPPA = {
 
+    Routers: {},
     Models: {},
     Collections: {},
     Views: {},
     init: function() {
+      // Initialize router
+      this.mainRouter = new NPPA.Routers.MainRouter();
+      Backbone.history.start();
     }
 
   };
 
+  // Main router
+  NPPA.Routers.MainRouter = Backbone.Router.extend({ 
+
+    routes: {
+      '': 'introView', // Landing page
+      'photos/:photoNumber': 'photoView' // Individual Photo view
+    },
+
+    introView: function() {
+      $('#app').html('Welcome!');
+    },
+
+    photoView: function(photoNumber) {
+      var currentPhoto = nppaImages[(photoNumber - 1)];
+
+      var Photo = new NPPA.Views.Photo({ photoUrl: currentPhoto });
+    }
+  
+  });
+
+
+  // User model
+  NPPA.Models.User = Backbone.Model.extend({
+
+    //attributes: {
+    //  firstName: '',
+    //  lastName: '',
+    //  qualityRatings: [],
+    //  shareRatings: []  
+    //}
+
+  });
+
+
+  // Users collection
+  NPPA.Collections.Users = Backbone.Collection.extend({
+
+    model: NPPA.Models.User
+
+  });
+
+
+  // Photo view
+  NPPA.Views.Photo = Backbone.View.extend({
+
+    el: '#app',
+
+    template: _.template($('#photo-template').html()),
+
+    initialize: function(options) {
+      this.photoUrl = options.photoUrl;
+      this.render();
+    },
+
+    render: function() {
+      this.$el.html(this.template({ photo: this.photoUrl }));
+    }
+
+  });
+
 }) ();
+
+$(document).ready(function() {
+
+  // Start the app
+  NPPA.init();
+
+});
---
+++ js/img-src.js
@@ -0,0 +1,5 @@
+nppaImages = [
+	'001_pro_news1.jpg', 
+	'002_pro_feature1.jpg', 
+	'003_pro_sports1.jpg'
+];
